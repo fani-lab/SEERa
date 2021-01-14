6cc63f455e6b465914008a9d715f9048c5f3ae66
Soroush Ziaeinejad <soroushziaeinejad@gmail.com>
2021-01-14 17:32:43 +0330

    Temporality added. Each documents is a collection of all daily tweets of a user

--- DataPreparation.py
+++ DataPreparation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import gensim
+import numpy as np
 from DataReader import DataReader
 import tagme
 from gensim.parsing.preprocessing import STOPWORDS
@@ -13,13 +14,29 @@ def data_preparation(userModeling=True, timeModeling=True,  preProcessing=False,
     global DataLen
     print('data_preparation')
     dataset = DataReader()
+    creation_dates = []
+    creation_times = []
+    for i in dataset[:, 2]:
+        creation_dates.append(i.date())
+        creation_times.append(i.time())
+    creation_dates = np.asarray(creation_dates)
+    creation_times = np.asarray(creation_times)
+    # data = pd.DataFrame(
+    #     {'Id': dataset[:, 0], 'Text': dataset[:, 1], 'CreationTimeStamp': dataset[:, 2], 'userId': dataset[:, 3],
+    #      'ModificationTimeStamp': dataset[:, 4]})
     data = pd.DataFrame(
-        {'Id': dataset[:, 0], 'Text': dataset[:, 1], 'CreationTimeStamp': dataset[:, 2], 'userId': dataset[:, 3],
-         'ModificationTimeStamp': dataset[:, 4]})
+             {'Id': dataset[:, 0], 'Text': dataset[:, 1], 'CreationDate': creation_dates, 'CreationTimes': creation_times, 'userId': dataset[:, 3],
+              'ModificationTimeStamp': dataset[:, 4]})
     # data = data.sample(frac=1)
-    data = data[:150000]
-    if userModeling:
+    data = data[:8000]
+    if userModeling and timeModeling:
+        dataGroupbyUsers = data.groupby(['userId', 'CreationDate'])
+        documents = dataGroupbyUsers['Text'].apply(lambda x: '\n'.join(x)).reset_index()
+    elif userModeling:
         dataGroupbyUsers = data.groupby(['userId'])
+        documents = dataGroupbyUsers['Text'].apply(lambda x: '\n'.join(x)).reset_index()
+    elif timeModeling:
+        dataGroupbyUsers = data.groupby(['CreationDate'])
         documents = dataGroupbyUsers['Text'].apply(lambda x: '\n'.join(x)).reset_index()
     else:
         data_text = data[['Text']]
