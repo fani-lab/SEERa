c380187f680f6cc543ca26101142640bda7d044f
hosseinfani <hosseinfani@gmail.com>
2022-07-22 20:53:40 -0400

    no message

--- src/cpl/GraphClustering.py
+++ src/cpl/GraphClustering.py
@@ -38,9 +38,9 @@ def main(embeddings, path2save, method='louvain', temporal=False):
     UserSimilarityThreshold = params.uml['UserSimilarityThreshold']
     pred_usersSimilarity = pairwise_kernels(embeddings[-1, :, :], metric='cosine', n_jobs=9)
     pred_usersSimilarity[pred_usersSimilarity < UserSimilarityThreshold] = 0
-    #pred_usersSimilarity[random.randint(pred_usersSimilarity.shape[0]*pred_usersSimilarity.shape[1]-100)] == 0
-    pred_usersSimilarity = np.random.random(pred_usersSimilarity.shape)
-    pred_usersSimilarity[pred_usersSimilarity < 0.99] = 0
+    # pred_usersSimilarity[random.randint(pred_usersSimilarity.shape[0]*pred_usersSimilarity.shape[1]-100)] == 0
+    # pred_usersSimilarity = np.random.random(pred_usersSimilarity.shape)
+    # pred_usersSimilarity[pred_usersSimilarity < 0.99] = 0
     pred_usersSimilarity = sparse.csr_matrix(pred_usersSimilarity)
 
     cmn.logger.info(f'5.2. Future Graph Prediction ...')#potential bottleneck if huge amount of edges! needs large filtering threshold
