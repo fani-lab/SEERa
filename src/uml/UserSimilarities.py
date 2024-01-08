da2241d0760b8d3f8367dd5c980f9dc8b6a00b6e
Soroush Ziaeinejad <soroushziaeinejad@gmail.com>
2024-01-08 02:13:34 -0500

    code cleaup + Improved speed

--- src/uml/UserSimilarities.py
+++ src/uml/UserSimilarities.py
@@ -1,10 +1,7 @@
 import datetime
 import os
-import numpy as np
 import warnings
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
-import networkx as nx
-import pandas as pd
 
 import params
 from cmn import Common as cmn
@@ -13,27 +10,15 @@ from uml import UsersGraph as UG
 
 def main(documents, dictionary, lda_model):
     if not os.path.isdir(params.uml['path2save']): os.makedirs(params.uml['path2save'])
-
-    documents['TopicInterests'] = pd.Series
-    for index, row in documents.iterrows():
-        user_bow_corpus = dictionary.doc2bow(row['Tokens'])
-        documents.loc[index, 'TopicInterests'] = str(list(tm.doc2topics(lda_model, user_bow_corpus)))
+    documents['TopicInterests'] = documents['Tokens'].apply(lambda tokens: str(list(tm.doc2topics(lda_model, dictionary.doc2bow(tokens)))))
     if not os.path.isdir(f'{params.uml["path2save"]}/graphs'): os.makedirs(f'{params.uml["path2save"]}/graphs')
     if not os.path.isdir(f'{params.uml["path2save"]}/user_interests'): os.makedirs(f'{params.uml["path2save"]}/user_interests')
     end_date = datetime.datetime.strptime(params.dal['end'], '%Y-%m-%d')
     day = datetime.datetime.strptime(params.dal['start'], '%Y-%m-%d')
-    startTimeStamp = 0
     endTimeStamp = (end_date - day).days // params.dal['timeInterval']
-    timeStamp = startTimeStamp
-    len_users = []
-    connections_path = f'{params.dal["path"]}/user_connections.csv'
-    while timeStamp <= endTimeStamp:
-        cmn.logger.info(f'{(documents["TimeStamp"] == timeStamp).sum()} users have twitted in {day+datetime.timedelta(timeStamp)}')
-        len_users.append(documents[documents['TimeStamp'] == timeStamp]['UserId'].nunique())
-        timeStamp += 1
+    len_users = [documents[documents['TimeStamp'] == ts]['UserId'].nunique() for ts in range(0, endTimeStamp + 1)]
     cmn.logger.info(f'UserSimilarity: Number of users per day: {len_users}')
-
-    graphs = UG.graph_generator(documents, connections_path)
+    graphs = UG.graph_generator(documents)
     cmn.logger.info(f'UserSimilarity: Graphs are written in "graphs" directory')
     return graphs
 
--- src/uml/UsersGraph.py
+++ src/uml/UsersGraph.py
@@ -1,65 +1,35 @@
-import networkx as nx
-from scipy import sparse
-#import numpy as np
-#import sys
-import pandas as pd
 import torch
 import numpy as np
-import torch_geometric
-import torch_geometric_temporal
 from torch_geometric_temporal.signal import StaticGraphTemporalSignal as sgts
-from sklearn.metrics.pairwise import cosine_similarity, pairwise_kernels
 
 import params
 from cmn import Common as cmn
 
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, num_timesteps, features, edge_index):
-        self.num_nodes = features.shape[0]
-        self.num_features = features.shape[1]
-        self.num_timesteps = num_timesteps
-        self.edge_index = edge_index
-
-        edges = edge_index
-        # Remove self-loops and duplicate edges
-        edges = edges[:, edges[0] != edges[1]]
-        edges = torch.unique(edges, dim=1)
-
-        edges_weight = torch.ones(edges.shape[1])
-        x_list, y_list = [], []
-        for t in range(num_timesteps-1):
-
-            x_list.append(features[t])
-            y_list.append(features[t+1])
-        self.data = sgts(np.asarray(edges), np.asarray(edges_weight), x_list, y_list)
-
+        self.num_nodes, self.num_features, self.num_timesteps = features.shape[0], features.shape[1], num_timesteps
+        self.edge_index = torch.unique(edge_index[:, edge_index[0] != edge_index[1]], dim=1)
+        self.edges_weight = torch.ones(self.edge_index.shape[1])
+        x_list = [features[t] for t in range(num_timesteps - 1)]
+        y_list = [features[t + 1] for t in range(num_timesteps - 1)]
+        self.data = sgts(np.asarray(self.edge_index), np.asarray(self.edges_weight), x_list, y_list)
     def __len__(self):
         return self.num_nodes
-
     def __getitem__(self, idx):
         return self.data
-
-def graph_generator(documents, connections_path):
+def graph_generator(documents):
     all_nodes =  documents['UserId'].unique()
-
     num_random_edges = 5 * len(all_nodes)
-    edges = torch.randint(0, all_nodes.shape[0], (2, num_random_edges), dtype=torch.long)
-    # Remove self-loops and duplicate edges
+    edges = torch.randint(0, len(all_nodes), (2, num_random_edges), dtype=torch.long)
     edges = edges[:, edges[0] != edges[1]]
     edges = torch.unique(edges, dim=1)
-    edges_weight = torch.ones(edges.shape[1])
-    # generating feature matrix
     num_timeStamps = documents['TimeStamp'].max() - documents['TimeStamp'].min()
-    num_features = params.tml['numTopics']
-
-    features = np.zeros((num_timeStamps, all_nodes.shape[0], num_features))
+    features = np.zeros((num_timeStamps, len(all_nodes), params.tml['numTopics']))
     for t in range(num_timeStamps):
         documents_t = documents[documents['TimeStamp'] == t]
-        for index, user_id in enumerate(all_nodes):
-            if user_id in documents_t['UserId'].values:
-                features[t][index] = np.asarray(eval(documents_t.loc[documents_t['UserId'] == user_id, 'TopicInterests'].iloc[0]))
-
-    print (features.shape)
+        user_ids_t = documents_t['UserId'].values
+        indices = np.isin(all_nodes, user_ids_t)
+        features[t, indices] = np.asarray([eval(documents_t[documents_t['UserId'] == user_id]['TopicInterests'].iloc[0]) for user_id in all_nodes[indices]])
     dataset = MyDataset(num_timeStamps, features, edges)
     import pickle
     with open(f"{params.uml['path2save']}/graphs/graphs.pkl", 'wb') as f:
