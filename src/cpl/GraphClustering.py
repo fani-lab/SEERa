cd6a0b08ff52721a0dbe77588830270cae35b1b1
Soroush Ziaeinejad <soroushziaeinejad@gmail.com>
2024-01-13 06:53:16 -0500

    Added AgglomerativeClustering and Louvain clustering methods

--- src/cpl/GraphClustering.py
+++ src/cpl/GraphClustering.py
@@ -23,7 +23,60 @@ def main2(user_features, predicted_features):
                 pickle.dump(labels, f)
             user_clusters.to_csv(f'{params.cpl["path2save"]}/user_clusters.csv')
             return user_clusters, labels
+        elif params.cpl['method'] == 'AgglomerativeClustering':
+            from sklearn.cluster import AgglomerativeClustering
+            from sklearn.metrics.pairwise import cosine_similarity
+            similarities = cosine_similarity(predicted_features, predicted_features)
+            agglomerative = AgglomerativeClustering(metric='precomputed', linkage='average')
+            labels = agglomerative.fit_predict(similarities)
+            agglomerative2 = AgglomerativeClustering(metric='cosine', linkage='average')
+            labels2 = agglomerative2.fit_predict(similarities)
+            agglomerative3 = AgglomerativeClustering(metric='l2', linkage='average')
+            labels3 = agglomerative3.fit_predict(similarities)
+            # assert labels==labels2
 
+            user_clusters = pd.DataFrame({'UserId': user_features['UserId'], 'Community': labels.tolist()})
+            with open(f"{params.cpl['path2save']}/PredUserClusters.npy", 'wb') as f:
+                pickle.dump(labels, f)
+            user_clusters.to_csv(f'{params.cpl["path2save"]}/user_clusters.csv')
+
+            user_clusters2 = pd.DataFrame({'UserId': user_features['UserId'], 'Community': labels2.tolist()})
+            with open(f"{params.cpl['path2save']}/PredUserClusters2.npy", 'wb') as f:
+                pickle.dump(labels2, f)
+            user_clusters2.to_csv(f'{params.cpl["path2save"]}/user_clusters2.csv')
+
+            user_clusters3 = pd.DataFrame({'UserId': user_features['UserId'], 'Community': labels3.tolist()})
+            with open(f"{params.cpl['path2save']}/PredUserClusters3.npy", 'wb') as f:
+                pickle.dump(labels3, f)
+            user_clusters3.to_csv(f'{params.cpl["path2save"]}/user_clusters3.csv')
+
+
+            return user_clusters, labels
+    elif params.cpl['type'] == 'graph_based':
+        if params.cpl['method'] == 'louvain':
+            from sknetwork.clustering import Louvain
+            # louvain = Louvain(resolution=1, n_aggregations=200, shuffle_nodes=True, return_membership=True,
+            #                       return_aggregate=True, verbose=True)
+            # louvain2 = Louvain(resolution=1, max_agg_iter=200, shuffle_nodes=True, verbose=1)
+            louvain = Louvain(resolution=1, shuffle_nodes=True, verbose=1)
+            from sklearn.metrics.pairwise import cosine_similarity
+            similarities = cosine_similarity(predicted_features, predicted_features)
+            np.save(f'{params.cpl["path2save"]}/adj.npy', similarities)
+            cmn.logger.info(f"(size) : ({similarities.size})")
+            cmn.logger.info(f"(#zeros) : ({similarities.size - np.count_nonzero(similarities)})")
+            adj2 = sparse.csr_matrix(similarities)
+            pd.to_pickle(adj2, f'{params.cpl["path2save"]}/adj_sparse.pkl')
+            labels = np.asarray(louvain.fit_transform(adj2))
+            argmax_indices_dense = np.asarray(np.argmax(labels.tolist().todense(), axis=1).flatten())[0]
+            user_clusters = pd.DataFrame({'UserId': user_features['UserId'], 'Community': argmax_indices_dense})
+            with open(f"{params.cpl['path2save']}/PredUserClusters.npy", 'wb') as f:
+                pickle.dump(labels, f)
+            user_clusters.to_csv(f'{params.cpl["path2save"]}/user_clusters.csv')
+            return user_clusters, labels
+
+            # np.save(f'{params.cpl["path2save"]}/PredUserClusters.npy', labels)
+            # print(labels)
+            # pd.DataFrame(labels).to_csv(f'{params.cpl["path2save"]}/user_clusters.csv')
 def cluster_topic_interest(clusters, user_topic_interests):
     cluster_interests = []
     for i in range(clusters.max()+1):
